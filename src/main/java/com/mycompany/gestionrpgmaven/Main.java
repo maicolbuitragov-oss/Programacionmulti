9dc5ea575f4c6a8b88abdf744fce951da4bc0a2d
Kevin Buitrago <maicol.buitrago.v@gmail.com>
2026-09-08 23:49:24 -0500

    Finaliza gestion de personajes con Javalin

--- src/main/java/com/mycompany/gestionrpgmaven/Main.java
+++ src/main/java/com/mycompany/gestionrpgmaven/Main.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.gestionrpgmaven;
 
 import com.mycompany.gestionrpgmaven.Controlador.PersonajeControlador;
@@ -11,12 +7,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Javalin app = Javalin.create().start(7070);
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+        }).start(7077);
 
         app.get("/personajes", PersonajeControlador::obtenerPersonajes);
 
         app.post("/personajes", PersonajeControlador::crearPersonaje);
 
-        System.out.println("Servidor iniciado en http://localhost:7070");
+        System.out.println("Servidor iniciado en http://localhost:7077");
     }
 }
